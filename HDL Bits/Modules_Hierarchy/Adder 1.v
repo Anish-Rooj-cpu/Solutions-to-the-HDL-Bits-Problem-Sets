da145f252669d7703c0486cf1708f4315d556818
Anish-Rooj-cpu <anishrooj005@gmail.com>
2025-04-17 11:14:16 +0530

    Update Adder 1.v

---
+++ HDL Bits/Modules_Hierarchy/Adder 1.v
@@ -0,0 +1,10 @@
+module top_module(
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);
+    wire w0;
+    add16 add0(.a(a[15:0]), .b(b[15:0]), .cin(1'b0), .sum(sum[15:0]), .cout(w0));
+    add16 add1(.a(a[31:16]), .b(b[31:16]), .cin(w0), .sum(sum[31:16]), .cout(1'bz));
+    
+endmodule
